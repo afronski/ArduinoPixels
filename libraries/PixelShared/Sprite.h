3808f0405945fba40275f8bdb0ae76569b414ba2
Michael Kötter <michael@m-koetter.de>
2014-03-25 19:02:20 +0100

    fix cubicEaseInOut function

--- libraries/PixelShared/Sprite.h
+++ libraries/PixelShared/Sprite.h
@@ -150,9 +150,9 @@ public:
     }
 
     static float cubicEaseInOut(float t,float b , float c, float d) {
-	t = t/d/2;
+	t/=d/2;
         if (t < 1) return c/2*t*t*t + b;
-	t -= 2;
+	t-=2;
         return c/2*(t*t*t + 2) + b;
     }
 
