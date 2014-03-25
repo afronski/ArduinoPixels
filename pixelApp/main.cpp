0eefb64b66d1a8585549b0f500fe90f61049b016
Michael Kötter <michael@m-koetter.de>
2014-03-25 18:48:53 +0100

    handle input (1p only for now)

--- pixelApp/main.cpp
+++ pixelApp/main.cpp
@@ -16,8 +16,35 @@
 
 #include "PixelMain.h"
 
+// buttons / joysticks
+#define BTN_BLUE	0
+#define BTN_RED		2
+#define BTN_YELLOW 	4
+
+#define JS_NEUTRAL	0
+#define JS_UP		1
+#define JS_UP_LEFT	8
+#define JS_UP_RIGHT	2
+#define JS_DOWN		5
+#define JS_DOWN_LEFT	6
+#define JS_DOWN_RIGHT	4
+#define JS_LEFT		7
+#define JS_RIGHT	3
+
+#define OFFSET_BTN_R	0
+#define OFFSET_BTN_L	20
+#define OFFSET_JS_R	10
+#define OFFSET_JS_L	30
+
 using namespace std;
 using namespace std::chrono;
+
+struct joystickState {
+	bool up, down, left, right;
+} inputL, inputR;
+
+void handleKeyEvent(PixelMain &main, SDL_KeyboardEvent *key);
+void handleJoystick(PixelMain &main, joystickState &input, int offset);
 
 int main(int argc, char** argv) {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -32,12 +59,18 @@ int main(int argc, char** argv) {
 
 	bool running = true;
 	SDL_Event event;
-	int fps = 30;
+	int fps = 20;
 
 	while (running) {
 		while(SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
+			switch (event.type) {
+			case SDL_QUIT:
 				running = false;
+				break;
+			case SDL_KEYDOWN:
+			case SDL_KEYUP:
+				handleKeyEvent(pixelMain, &event.key);
+				break;
 			}
 		}
 
@@ -50,4 +83,67 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
+void handleKeyEvent(PixelMain &main, SDL_KeyboardEvent *key) {
+	// layout:
+	// left, right, up, down, comma, period, slash control player 1 (~right controls)
+	bool pressed = (key->type == SDL_KEYDOWN);
 
+	switch( key->keysym.sym ){
+	case SDLK_ESCAPE:
+		main.setGameState(STATE_INTRO);
+		break;            
+	case SDLK_COMMA:
+		main.setInput(OFFSET_BTN_R + BTN_RED + (pressed ? 1 : 0));
+		break;
+	case SDLK_PERIOD:
+		main.setInput(OFFSET_BTN_R + BTN_BLUE + (pressed ? 1 : 0));
+		break;
+	case SDLK_MINUS:
+		main.setInput(OFFSET_BTN_R + BTN_YELLOW + (pressed ? 1 : 0));
+		break;
+	case SDLK_UP:
+		inputR.up = pressed;
+		handleJoystick(main, inputR, OFFSET_JS_R);
+		break;
+	case SDLK_DOWN:
+		inputR.down = pressed;
+		handleJoystick(main, inputR, OFFSET_JS_R);
+		break;
+	case SDLK_LEFT:
+		inputR.left = pressed;
+		handleJoystick(main, inputR, OFFSET_JS_R);
+		break;
+	case SDLK_RIGHT:
+		inputR.right = pressed;
+		handleJoystick(main, inputR, OFFSET_JS_R);
+		break;		
+	default:
+		break;
+	}
+	
+
+}
+
+void handleJoystick(PixelMain &main, joystickState &input, int offset) {
+	int js = offset + JS_NEUTRAL;
+	if (input.up) {
+		if (input.left)
+			js += JS_UP_LEFT;
+		else if (input.right)
+			js += JS_UP_RIGHT;
+		else
+			js += JS_UP;
+	} else if (input.down) {
+		if (input.left)
+			js += JS_DOWN_LEFT;
+		else if (input.right)
+			js += JS_DOWN_RIGHT;
+		else
+			js += JS_DOWN;
+	} else if (input.left) {
+		js += JS_LEFT;
+	} else if (input.right) {
+		js += JS_RIGHT;
+	}
+	main.setInput(js);
+}
